452c6ec930307cb31466f42783aa709870b72f0f
yingjian.chen <yingjian.chen@vegagame.com>
2022-12-22 15:47:00 +0800

    xcode更新到13.4.1 需要根据readme重新生成工程

--- xcode/LearnOpenGL.build/Debug/1.getting_started__1.2.hello_window_clear.build/Objects-normal/x86_64/hello_window_clear.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/vega/Documents/learnOpenGL/src/1.getting_started/1.2.hello_window_clear/hello_window_clear.cpp \
-  /Users/vega/Documents/learnOpenGL/includes/glad/glad.h \
-  /Users/vega/Documents/learnOpenGL/includes/KHR/khrplatform.h \
-  /Users/vega/Documents/learnOpenGL/includes/GLFW/glfw3.h
